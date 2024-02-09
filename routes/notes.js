c056fdbd8e406cf4df31adffa9ed7e1129a42bfa
Mel Mel Mom <melmelmomof4@gmail.com>
2024-02-09 14:46:49 -0500

    getting everything set up with classmates

---
+++ routes/notes.js
@@ -0,0 +1,14 @@
+
+const notesRouter = require('express').Router()
+
+
+notesRouter.get('/', (req, res) => {
+    // console.log('Something')
+})
+
+
+notesRouter.post('/', (req, res) => {
+    console.log(req.body)
+})
+
+module.exports = notesRouter
