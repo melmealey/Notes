08a1a576565a175cc634bfdbc5516457be66a75a
Mel Mel Mom <melmelmomof4@gmail.com>
2024-02-10 15:35:59 -0500

    new notes saving

--- routes/notes.js
+++ routes/notes.js
@@ -1,14 +1,33 @@
-
-const notesRouter = require('express').Router()
+const notesRouter = require('express').Router();
+const { readFromFile, readAndAppend } =require ('../helpers/fs-utils')
+const uuid = require('../helpers/uuid');
 
 
 notesRouter.get('/', (req, res) => {
-    // console.log('Something')
+    console.info(`${req.method} request received for tips`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 })
 
-
 notesRouter.post('/', (req, res) => {
-    console.log(req.body)
+    console.info(`${req.method} request received to add a tip`);
+    console.log(req.body);
+  
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        note_id: uuid(),
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+      res.json(`Note added successfully`);
+    } else {
+      res.error('Error in adding note');
+    }
 })
 
 module.exports = notesRouter
+
+
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const fs = require('fs')
 const path = require('path')
 
-const PORT = 3002
+const PORT = 3333
 
 const app = express()
 
@@ -23,6 +23,7 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 )
 
+//Starts the server to begin listening
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 )
